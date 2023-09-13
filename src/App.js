47864ee685786c094468cc1bf1c7255c5ca622fb
praneeth <kodadalajp0503@gmail.com>
2023-09-14 00:40:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import './App.css';
+import Alerts from './components/Alerts';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import React ,{useState} from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  // alert
+  const [alert, setAlert] = useState(null)
+  const showAlert = (message, alertType) => {
+    setAlert({
+      msg: message,
+      type: alertType,
+    })
+    setTimeout(() => {
+      showAlert(null)
+    }, 1000);
+  }
+  // dark mode
   const [mode, setMode] = useState('light');
-  const toggleMode=()=>{
-    if(mode==='light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor='gray';
+      document.body.style.backgroundColor = 'gray';
+      showAlert("Dark mode Enabled", "success");
+      setTimeout(() => {
+        
+      }, 1500);
     }
-    else{
+    else {
       setMode('light');
-      document.body.style.backgroundColor='white';
+      document.body.style.backgroundColor = 'white';
+      showAlert("Dark mode Disabled", "warning");
     }
   }
   return (
     <>
-    <Navbar title="TextHelper" mode={mode} toggleMode={toggleMode}/>
-    <TextForm mode={mode}/>
+      <Navbar title="TextHelper" mode={mode} toggleMode={toggleMode} />
+      <Alerts alert={alert}>
+
+      </Alerts>
+      <TextForm mode={mode} showAlert={showAlert}/>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 
 
 export default function TextForm(props) {
-  const [text, setText] = useState(' ')
+  const [text, setText] = useState('')
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to UpperCase", "success");
   }
   // function to convert to upper case
   const handleOnChange = (event) => {
@@ -26,27 +27,27 @@ export default function TextForm(props) {
     var text = document.getElementById("box");
     text.select();
     navigator.clipboard.writeText(text.value);
+    props.showAlert("Copied to ClipBoard", "success");
   }
   return (
     <>
       <div className="container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
         <div className="mb-3">
-          <label htmlFor="box" className="form-label my-3"><h1>Enter here</h1></label>
-          <textarea className="form-control" id="box" onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'lightgray' : 'white' }} value={text} rows="7"></textarea>
+          <label htmlFor="box" className="form-label my-2"><h2>Enter here</h2></label>
+          <textarea className="form-control" id="box" onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark' ? 'lightgray' : 'white' }} value={text} rows="7"></textarea>
         </div>
         <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
         <button className="btn btn-primary mx-2" onClick={handleClear}>Clear</button>
         <button className="btn btn-primary" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
       </div>
       <div className="container mt-2" style={{color:props.mode==='dark'?'white':'black'}}>
-        <h1>Text Preview</h1>
+        <h2>Text Preview</h2>
         <p className='mb-1'>{text.split(" ").length} World and {text.length} Characters</p>
         <p className='mb-1'>{0.008 * text.split(" ").length} time to read</p>
-        <h2>Preview</h2>
-        <p>{text}</p>
+        <h3>Preview</h3>
+        <p>{text.length>0?text:"enter something to Preview"}</p>
         <button className="btn btn-primary" onClick={handleCopy}>Copy Preview</button>
       </div>
-
     </>
   )
 }
